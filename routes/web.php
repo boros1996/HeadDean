9bd9b47957b6d7eddc4887855706c85a051b6d42
boros1996 <aviidjaurigue07@gmail.com>
2023-11-17 18:43:46 +0800

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::middleware('is_admin')->prefix('admin')->group(function() {
+    Route::get('home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home');
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
